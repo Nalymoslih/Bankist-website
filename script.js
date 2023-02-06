cd21d7ee0d3476a67eca20f157b9f040dc8b6ee4
macbookshop <naly.moslih48@gmail.com>
2023-02-06 23:01:23 +0300

    update

--- script.js
+++ script.js
@@ -132,3 +132,10 @@ btnScrollTo.addEventListener('click', function (e) {
 
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+
+const h1 = document.querySelector('h1');
+const alertH1 = function (e) {
+  alert('addEventListner: Great');
+};
+h1.addEventListener('mouseenter', alertH1);
+setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000);
