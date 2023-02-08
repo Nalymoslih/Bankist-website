b3676cfe9b0f7b34cc75ecacdeab2eb57a6775e7
macbookshop <naly.moslih48@gmail.com>
2023-02-08 22:48:23 +0300

    test

--- script.js
+++ script.js
@@ -80,7 +80,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   // Matching strategy
   if (e.target.classList.contains('nav__link')) {
     const id = e.target.getAttribute('href');
-    console.log(id);
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
 });
