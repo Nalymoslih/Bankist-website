969e9ce0014f0cef611094979c21ead615820462
macbookshop <naly.moslih48@gmail.com>
2023-02-10 23:46:26 +0300

    test

--- script.js
+++ script.js
@@ -84,6 +84,14 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+// Tabbed component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operation__content');
+
+// Bad Practice
+// tabs.forEach(t => t.addEventListener('click', () => console.log('TAB')));
+
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Creating and inserting element
