ed8c9cccdfbee726993c9a3d4a09ed3ea83545b0
macbookshop <naly.moslih48@gmail.com>
2023-02-11 07:56:21 +0300

    update

--- script.js
+++ script.js
@@ -96,7 +96,18 @@ tabsContainer.addEventListener('click', function (e) {
   // Guard clause
   if (!clicked) return;
 
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
   clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  console.log(clicked.dataset.tab);
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
 
 // Bad Practice
